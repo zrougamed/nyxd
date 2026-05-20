520106320c541acb35fca63254b1d1c16011194a
Mohamed Zrouga <zrouga.mohammed@gmail.com>
2026-05-20 22:56:04 +0100

    domain update

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -16,10 +16,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://zrougamed.github.io',
+  url: 'https://nyxd.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/nyxd/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
